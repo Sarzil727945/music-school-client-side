297e48c600fa93d88e54f031418f38f6dd194f42
Sarzil727945 <sarzilmuntaha.gmail.com>
2023-06-11 22:13:12 +0600

    home page complete

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,12 +3,14 @@ import useTitle from '../../hooks/useTitle';
 import Slider from './Slider/Slider';
 import PopularInstructors from './PopularInstructors/PopularInstructors';
 import Service from './Service/Service';
+import PopularClasses from './PopularClasses/PopularClasses';
 
 const Home = () => {
      useTitle('Home')
      return (
           <div className=' my-5 py-5'>
                <Slider></Slider>
+               <PopularClasses></PopularClasses>
                <PopularInstructors></PopularInstructors>
                <Service></Service>
           </div>
